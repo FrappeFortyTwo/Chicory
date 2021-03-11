e930d0ad801b599bd04de0861648748c25e08175
Sourav Singh <sourav.singh.dev@gmail.com>
2021-03-12 00:43:03 +0530

    refactored code

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -74,81 +72,85 @@ func main() {
 	body, err := ioutil.ReadAll(resp.Body)
 	checkErr(err, "Unable to read response body")
 
+	println(resp.Status)
+
 	// regex to fetch video urls & meta data
+	bodyStr := string(body)
 	re := regexp.MustCompile(`"adaptiveFormats"+.+\]\},"playerAds"`)
 
-	// process data into json format
-	vs := strings.Replace(string(re.FindAll(body, -1)[0]), "\"adaptiveFormats\":", "{ \"formats\":", 1)
-	vs = strings.Replace(vs, "},\"playerAds\"", "}", 1)
+	
+	// // process data into json format
+	// vs := strings.Replace(string(re.FindAll(body, -1)[0]), "\"adaptiveFormats\":", "{ \"formats\":", 1)
+	// vs = strings.Replace(vs, "},\"playerAds\"", "}", 1)
 
-	// dump contents into file
-	err = ioutil.WriteFile("temp.json", []byte(vs), 0777)
-	checkErr(err, "Unable to write to temp.json")
+	// // dump contents into file
+	// err = ioutil.WriteFile("temp.json", []byte(vs), 0777)
+	// checkErr(err, "Unable to write to temp.json")
 
-	// read file as json
-	jsonFile, err := os.Open("temp.json")
-	checkErr(err, "Unable to read temp.json")
-	defer jsonFile.Close()
+	// // read file as json
+	// jsonFile, err := os.Open("temp.json")
+	// checkErr(err, "Unable to read temp.json")
+	// defer jsonFile.Close()
 
-	byteVal, _ := ioutil.ReadAll(jsonFile)
+	// byteVal, _ := ioutil.ReadAll(jsonFile)
 
-	// initialise formats ~ various formats the video at provided url is available in
-	var formats Formats
+	// // initialise formats ~ various formats the video at provided url is available in
+	// var formats Formats
 
-	// unmarshal contents
-	json.Unmarshal(byteVal, &formats)
+	// // unmarshal contents
+	// json.Unmarshal(byteVal, &formats)
 
-	// audio index
-	var audioStartIndex int
+	// // audio index
+	// var audioStartIndex int
 
-	// iterate through every format and print respective meta data
-	println("Option\t|\tItag\t|\tType\t\t|\tQuality\n")
-	for i := 0; i < len(formats.Formats); i++ {
+	// // iterate through every format and print respective meta data
+	// println("Option\t|\tItag\t|\tType\t\t|\tQuality\n")
+	// for i := 0; i < len(formats.Formats); i++ {
 
-		// if meme type contains audio ~ break
-		if strings.Contains(formats.Formats[i].MimeType, "audio") {
-			audioStartIndex = i
-			break
-		}
+	// 	// if meme type contains audio ~ break
+	// 	if strings.Contains(formats.Formats[i].MimeType, "audio") {
+	// 		audioStartIndex = i
+	// 		break
+	// 	}
 
-		// display options to download video
-		tmpA := strings.Split(formats.Formats[i].MimeType, "; ")
-		println(i, "\t|\t", formats.Formats[i].Itag, "\t|\t", tmpA[0], "\t|\t", formats.Formats[i].QualityLabel, "\n")
-	}
+	// 	// display options to download video
+	// 	tmpA := strings.Split(formats.Formats[i].MimeType, "; ")
+	// 	println(i, "\t|\t", formats.Formats[i].Itag, "\t|\t", tmpA[0], "\t|\t", formats.Formats[i].QualityLabel, "\n")
+	// }
 
-	// input option to download video
-	print("Enter Option: ")
-	var input int
-	fmt.Scanln(&input)
+	// // input option to download video
+	// print("Enter Option: ")
+	// var input int
+	// fmt.Scanln(&input)
 
-	// making channel of type string
-	c := make(chan bool)
+	// // making channel of type string
+	// c := make(chan bool)
 
-	// fetch formats to for ffmpeg arguments
-	tmp := strings.Split(formats.Formats[input].MimeType, "; ")
-	videoFormat := strings.Replace(tmp[0], "video/", "", -1)
+	// // fetch formats to for ffmpeg arguments
+	// tmp := strings.Split(formats.Formats[input].MimeType, "; ")
+	// videoFormat := strings.Replace(tmp[0], "video/", "", -1)
 
-	tmp = strings.Split(formats.Formats[audioStartIndex].MimeType, "; ")
-	audioFormat := strings.Replace(tmp[0], "audio/", "", -1)
+	// tmp = strings.Split(formats.Formats[audioStartIndex].MimeType, "; ")
+	// audioFormat := strings.Replace(tmp[0], "audio/", "", -1)
 
-	println("\nDownloading Video ...")
-	go fetchFile(c, formats.Formats[input].URL, "video."+videoFormat)
+	// println("\nDownloading Video ...")
+	// go fetchFile(c, formats.Formats[input].URL, "video."+videoFormat)
 
-	// fetch audio from url
-	println("Downloading Audio ...")
-	go fetchFile(c, formats.Formats[input+1].URL, "audio."+audioFormat)
+	// // fetch audio from url
+	// println("Downloading Audio ...", audioStartIndex)
+	// go fetchFile(c, formats.Formats[input+1].URL, "audio."+audioFormat)
 
-	if <-c && <-c {
-		// after both audio & video files are downloaded
+	// if <-c && <-c {
+	// 	// after both audio & video files are downloaded
 
-		// merge them :
-		go mergeFiles(c, videoFormat, audioFormat)
+	// 	// merge them :
+	// 	go mergeFiles(c, videoFormat, audioFormat)
 
-	}
+	// }
 
-	// wait for merge to complete
-	<-c
-	println("\nDownload Complete !")
+	// // wait for merge to complete
+	// <-c
+	// println("\nDownload Complete !")
 
 }
 
@@ -157,15 +159,15 @@ func fetchFile(c chan bool, url string, title string) {
 	// process url ~ replace u2600 to &
 	url = strings.Replace(url, "u0026", "&", -1)
 
-	// make http request &
-	resp, err := http.Get(url)
-	checkErr(err, "Unable to make http request")
-	defer resp.Body.Close()
-
-	// save response to file
+	// create file to save response
 	out, err := os.Create(title)
 	checkErr(err, "Unable to create asset")
 	defer out.Close()
+
+	// make http request
+	resp, err := http.Get(url)
+	checkErr(err, "Unable to make http request")
+	defer resp.Body.Close()
 
 	// dump data to temp file
 	io.Copy(out, resp.Body)
